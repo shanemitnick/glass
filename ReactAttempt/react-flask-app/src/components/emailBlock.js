b43c99fa98779c12d0a7330e6cb8cd333271a708
shanemitnick <shane.mitnick21@gmail.com>
2021-11-17 10:06:49 -0500

    timeBlock now no longer hard coded.
    - uses JavaScript Date Object to create time and format time.
    
    - uses State that is updated every second
    - Seconds is only there for us to see it being updated (will change 
    later, to show to Owen how state works)

--- ReactAttempt/react-flask-app/src/components/emailBlock.js
+++ ReactAttempt/react-flask-app/src/components/emailBlock.js
@@ -2,9 +2,10 @@ import React from "react";
 
 class EmailBlock extends React.Component {
 
-
   render() {
-    return <h1> dcfdfg BLOCK HERE </h1>;
+
+
+    return <h1> Email Block to Be here </h1>;
   }
 }
 
--- ReactAttempt/react-flask-app/src/components/timeBlock.js
+++ ReactAttempt/react-flask-app/src/components/timeBlock.js
@@ -3,11 +3,45 @@ import './../styles/timeBlock.css';
 
 class TimeBlock extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  getTime() {
+    // Returns the Time in the correct Format
+    const time = this.state.date;
+    const hours = time.getHours() % 12;
+    const minutes = ((time.getMinutes() < 10)? '0' + time.getMinutes() : time.getMinutes());
+    const seconds = ((time.getSeconds() < 10)? '0' + time.getSeconds() : time.getSeconds());
+    const meridium = ((time.getHours() < 12)? 'AM' : 'PM');
+
+    return hours + ":" + minutes + ' ' + seconds + ' ' + meridium;
+  }
+
+  getDate() {
+    // Returns the Day in the correct Format
+    const time = this.state.date;
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const month = monthNames[time.getMonth()];
+    const date = time.getDate();
+
+    return month + " " + date;
+  }
+
   render() {
     return (
       <div className='time-container'>
-          <div className="time-digits">9:05 PM</div>
-          <div className="time-date">Oct 30, 2021 </div>
+          <div className="time-digits"> { this.getTime() } </div>
+          <div className="time-date"> { this.getDate() } </div>
       </div>
 
     );
