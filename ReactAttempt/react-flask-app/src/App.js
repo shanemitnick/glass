a3b77f081e5e102c7f7492ef7d6eaef54acbfc1e
shanemitnick <shane.mitnick21@gmail.com>
2021-11-20 01:34:13 -0500

    Created Postiioning structure for mirror page

--- ReactAttempt/react-flask-app/src/App.js
+++ ReactAttempt/react-flask-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import TimeBlock from './components/timeBlock.js';
 import WeatherBlock from './components/weatherBlock.js';
 import EmailBlock from './components/emailBlock.js';
@@ -8,28 +8,24 @@ import EmailBlock from './components/emailBlock.js';
 class Mirror extends React.Component {
 
   render() {
-return (
-        <div className="App">
-          <TimeBlock />
-          <WeatherBlock />
-          <EmailBlock />
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reloaad.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      );
-  }
+      return (
+              <div className="mirror-contatiner">
+
+                <div className="top-left-element">
+                    <TimeBlock />
+                </div>
+
+                <div className="top-right-element">
+                    <WeatherBlock />
+                </div>
+
+                <div className="bottom-left-element">
+                  <EmailBlock />
+                </div>
+
+              </div>
+            );
+    }
 }
 
 export default Mirror;
