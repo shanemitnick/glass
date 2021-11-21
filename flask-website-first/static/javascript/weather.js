12eaa6afc380998df6e1edb358cfe7f92149a115
shanemitnick <shane.mitnick21@gmail.com>
2021-11-21 01:47:26 -0500

    File Structure Change

--- flask-website-first/static/javascript/weather.js
+++
@@ -1,3 +0,0 @@
-console.log("HI");
-console.log(document);
-console.log(document.querySelectorAll("p"));
--- flask-website-first/static/main.js
+++
@@ -1,8 +0,0 @@
-
-
-const doc = new Document();
-
-const dayOne = doc.querySelectorAll('div');
-console.log(dayOne);
-
-setInterval(clock, inc);
