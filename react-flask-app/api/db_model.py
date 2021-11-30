e549862ddb91db28158559ae232273c2afca6376
norrisja <62315388+norrisja@users.noreply.github.com>
2021-11-29 21:41:18 -0500

    adding db init file

---
+++ react-flask-app/api/db_model.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask import current_app as app
+
+
+db = SQLAlchemy(app)
+
+# Initialize the Users table
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    zipcode = db.Column(db.String(10), nullable=False, unique=False)
+    date_joined = db.Column(db.DateTime, nullable=False, unique=False, default=datetime.utcnow())
+    # google_key = db.Column()
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
