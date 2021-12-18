61f3c072e151f83073a238d8876a763e45dc1df9
shanemitnick <shane.mitnick21@gmail.com>
2021-12-18 16:37:21 -0500

    Form completed and getting new data. Ready to send data to backend.
    Need to do CSS still, but ready.

--- react-flask-app/src/pages/Test.js
+++ react-flask-app/src/pages/Test.js
@@ -1,42 +1,97 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "../styles/profile-page.css";
-import { Button, Input } from "antd";
+import { Form, Button, Input, Select } from "antd";
 import Profile from "../components/website-components/profile";
 import NavBar from "../components/website-components/navbar.js";
 
 
 const Test = () =>  {
+  const { Option } = Select;
+
+  const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+  };
 
   const { user } = useAuth0();
-  const { name, email } = user;
-  useEffect(() => {
-    fetch('/login', {method: 'POST',
-                               headers: {"Content-Type": "application/json"},
-                                      // , "Content-Type": "application/x-www-form-urlencoded"}
-                              body: JSON.stringify({'username': 'User', 'password': 'Password'})}
+  const [form] = Form.useForm();
 
-    ).then(res => res.json()).then(data => {
-        console.log(data);
-        console.log(data);
-    });
-    console.log("USER");
-    console.log(user);
-    console.log(email);
+  const [ data, setData ] = useState();
+  const  [ gotData, setGotData ] = useState(false);
+
+  useEffect(() => {
+    if (!gotData) {
+        fetch('/profile/settings', {method: 'POST',
+                                   headers: {"Content-Type": "application/json"},
+                                          // , "Content-Type": "application/x-www-form-urlencoded"}
+                                  body: JSON.stringify({'user_id': 1})}
+
+        ).then(res => res.json()).then(returnedData => {
+            setData(returnedData);
+            setGotData(true);
+        });
+    }
+
   });
 
+
+
+  const handleFormSubmit = (values: any) => {
+    console.log("SUBMTT");
+    console.log(values);
+  };
+
+
+
+  console.log(data);
       return (
         <div>
         <NavBar />
         <Profile />
-              <div className='loginContainer'>
-                <h1> hello world. </h1>
-                <Button type="dashed">
-                    Download
-                </Button>
+          {!gotData ?
+            <h1> loading </h1> :
 
-                <Input className="input-button" placeholder="zipcode" />
-                </div>
+                  <div className='loginContainer'>
+
+                    <Form {...layout} onFinish={handleFormSubmit}>
+                      <Form.Item label="First Name"name="first_name" >
+                        <Input placeholder={data.first_name}/>
+                      </Form.Item>
+
+                      <Form.Item label="Last Name"name="last_name" >
+                        <Input placeholder={data.last_name}/>
+                      </Form.Item>
+
+
+                      <Form.Item label="Zipcode"name="zipcode" >
+                        <Input placeholder={data.zipcode}/>
+                      </Form.Item>
+
+                      <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+                        <Select
+                          placeholder={data.news_section}
+
+                          allowClear
+                        >
+                          <Option value="sports">sports</Option>
+                          <Option value="politics">politics</Option>
+                          <Option value="other">other</Option>
+                        </Select>
+                      </Form.Item>
+
+
+                      <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                      </Form.Item>
+                    </Form>
+
+
+
+                    </div>
+                }
         </div>
             );
 
