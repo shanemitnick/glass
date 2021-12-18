cbf77f2be022e1f25195b375290d0b4e0d391f90
norrisja <62315388+norrisja@users.noreply.github.com>
2021-12-18 15:45:04 -0500

    able to change user settings in the db

---
+++ react-flask-app/api/models/db_columns.py
@@ -0,0 +1,12 @@
+
+Users_columns = ['first_name',
+                 'last_name',
+                 'email',
+                 'zipcode',
+                 'top_left',
+                 'top_center',
+                 'top_right'
+                 'bottom_left',
+                 'bottom_center',
+                 'bottom_right',
+                 'section']
--- react-flask-app/api/routes/settings.py
+++ react-flask-app/api/routes/settings.py
@@ -1,7 +1,9 @@
 import re
 from flask import current_app as app
 from flask import request, jsonify
-from models.db_model import Users
+from models.db_model import Users, db
+from models.db_columns import Users_columns
+from sqlalchemy import update
 
 
 @app.route('/profile/settings', methods=["GET", "POST"])
@@ -10,22 +12,36 @@ def show_and_update_settings():
     user_id = response['user_id']
     user = Users.find_by_user_id(user_id=user_id)
 
+    # Update any settings if necessary and track invalid settings passed
+    invalid_settings = []
+    if len(list(set(response.keys()) - {'user_id'})) != 0:
+        for setting in response.keys():
+            if setting != 'user_id':
+                if setting in Users_columns:
+                    setattr(user, setting, response[setting])
+                else: 
+                    invalid_settings.append(setting)
+
+        db.session.commit()
+
+    for setting in invalid_settings:
+        print(f'{setting} is an invalid setting. Could not process change.')
+
+    user = Users.find_by_user_id(user_id=user_id)
+
     USER_SETTINGS = {
-        'first_name': user.first_name,
-        'last_name': user.last_name,
-        'email': user.email,
-        'zipcode': user.zipcode,
-
-        'top_right': user.top_right,
-        'top_center': user.top_center,
-        'top_left': user.top_left,
-        'bottom_left': user.bottom_left,
-        'bottom_center': user.bottom_center,
-        'bottom_right': user.bottom_right,
-
-        'news_section': str(user.section)    
+        'user': {'first_name': user.first_name,
+                 'last_name': user.last_name,
+                 'email': user.email,
+                 'zipcode': user.zipcode},
+        'mirror': {'top_right': user.top_right,
+                  'top_center': user.top_center,
+                   'top_left': user.top_left,
+                   'bottom_left': user.bottom_left,
+                   'bottom_center': user.bottom_center,
+                   'bottom_right': user.bottom_right},
+        'other': {'section': str(user.section)}
     }
-
 
     return USER_SETTINGS
 
