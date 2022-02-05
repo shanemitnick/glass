ee58d9bcc22aa01e6e79d65f61ab0f91e8bfc554
O-7085 <80608530+owen7085@users.noreply.github.com>
2022-02-05 13:16:58 -0500

    Basic Calendar Block
    Built JS and CSS for a basic calendar block using Norris' google calendar route.
    Also corrected spelling errors in sports component.

--- react-flask-app/src/components/mirror-components/calendarBlock.js
+++ react-flask-app/src/components/mirror-components/calendarBlock.js
@@ -23,11 +23,22 @@ function CalendarBlock() {
 
   return (<div className="calendar-container"> 
               {!gotData ?
-                  <div> Loading </div> : 
-                  <div> Loaded </div>
-                  }
-              </div>)
+                  <div> Loading </div> :
+                  <div className='schedule-container'>
+                  <div className='upcoming'> Upcoming Events </div>
+                    {Object.keys(calendar.items).map((event) => (
+                        <div className='events' key={event}>
+                            <div className='event-details'>
+                                <div className='event-date'> {String(calendar.items[event].start.dateTime).slice(5,10)} </div>
+                                <div className='event-name'> {calendar.items[event].summary} </div>
+                                <div className='time'> {String(calendar.items[event].start.dateTime).slice(11,16)} - {String(calendar.items[event].end.dateTime).slice(11, 16)} </div>
+                            </div>
+                        </div>
+                    ))}
+                  </div>
               }
+           </div>)
+ }
 
 export default CalendarBlock
 
